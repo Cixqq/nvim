6d3a5d641183cdbb04434b13963545dcdbd04298
Cixqq <bitcirq@gmail.com>
2024-04-12 00:02:58 +0200

    Fixing a small issue

--- init.lua
+++ init.lua
@@ -860,6 +860,27 @@ require('lazy').setup({
       local harpoon = require 'harpoon'
 
       harpoon:setup {}
+      vim.keymap.set('n', '<leader>a', function()
+        harpoon:list():add()
+      end, { desc = 'Harpoon: Append current file' })
+      vim.keymap.set('n', '<C-e>', function()
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end, { desc = 'Harpoon: Show mark list' })
+
+      -- Only gigachads use Dvorak (TODO: Try Dvorak or Colemak)
+      vim.keymap.set('n', '<C-a>', function()
+        harpoon:list():select(1)
+      end, { desc = 'Harpoon: Jump to mark 1' })
+      vim.keymap.set('n', '<C-s>', function()
+        harpoon:list():select(2)
+      end, { desc = 'Harpoon: Jump to mark 2' })
+      vim.keymap.set('n', '<C-d>', function()
+        harpoon:list():select(3)
+      end, { desc = 'Harpoon: Jump to mark 3' })
+      vim.keymap.set('n', '<C-f>', function()
+        harpoon:list():select(4)
+      end, { desc = 'Harpoon: Jump to mark 4' })
+
       -- basic telescope configuration
       local conf = require('telescope.config').values
       local function toggle_telescope(harpoon_files)
